779e829f978cf55399dbcd5c349658903b8f6727
SmirnovaAlina <alinas008@gmail.com>
2019-04-29 18:04:19 -0700

    add more endpoints

--- flask_part.py
+++ flask_part.py
@@ -34,13 +34,12 @@ def welcome():
 
 # 4. Define what to do when a user hits the /about route
 @app.route("/api/v1.0/precipitation")
-def precipitation():
-    data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >'2016-08-23').order_by(Measurement.date).all()  
+def precipitation(start_date):
+    data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >=func.strtime("%Y-%m-%d", start_date).order_by(Measurement.date).all()  
     result_list =[]
     for d in data:
         prcp_dict = {}
         prcp_dict['date'] = d.prcp
-        prcp_dict['prcp'] = d.prcp
         result_list.append(prcp_dict)
     return jsonify(result_list)
 
@@ -56,17 +55,36 @@ def stations():
     return jsonify(result_list)
 
 @app.route("/api/v1.0/tobs")
-def tobs():
-    data = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >'2016-08-23').all()
+def tobs(start_date):
+    data = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= func.strtime("%Y-%m-%d", start_date).order_by(Measurement.date)).all()
     result_list = []
     for d in data:
         tobs_dict = {}
-        tobs_dict['date'] = result.date
-        tobs_dict['tobs'] = result.tobs
+        tobs_dict['date'] = d.date
+        tobs_dict['tobs'] = d.tobs
         result_list.append(tobs_dict)
 
     return jsonify(result_list)
 
+@app.route("/api/v1.0/<start_date>)
+def start_date(start_date):
+    starts_date_dict = {}
+    data = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date >= func.strtime("%Y-%m-%d", start_date).order_by(Measurement.date)).all()
+    data_list = list(np.ravel(data))
+    starts_date_dict["t-max"] = data_list[1]
+    starts_date_dict["t-min"] = data_list[0]
+    starts_date_dict["t-avg"] = data_list[2]
+    return jsonify(starts_date_dict)
+
+@app.route("/api/v1.0/<start_date>/<end_date>)
+def start_end_dates(start_date, end_date):
+    start_end_dates_dict = {}
+    data = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date >= func.strtime("%Y-%m-%d", start_date).filter(Measurement.date <= func.strtime("%Y-%m-%d", end_date).all()
+    data_list = list(np.ravel(data))
+    start_end_dates_dict["t-max"] = data_list[1]
+    start_end_dates_dict["t-min"] = data_list[0]
+    start_end_dates_dict["t-avg"] = data_list[2]
+    return jsonify(sstart_end_dates_dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
